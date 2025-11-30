59374d0bcc96a7a4427aadfcb166dd56f80d7b81
Calabras <164652443+Calabras@users.noreply.github.com>
2025-11-30 13:28:48 +0300

    Replace MPI_Bcast with MPI_Scatterv for data distribution and remove unused pic.jpg file

--- tasks/shilin_n_counting_number_sentences_in_line/mpi/src/ops_mpi.cpp
+++ tasks/shilin_n_counting_number_sentences_in_line/mpi/src/ops_mpi.cpp
@@ -6,6 +6,7 @@
 #include <cstddef>
 #include <string>
 #include <utility>
+#include <vector>
 
 #include "shilin_n_counting_number_sentences_in_line/common/include/common.hpp"
 
@@ -108,24 +109,37 @@ bool ShilinNCountingNumberSentencesInLineMPI::RunImpl() {
     return true;
   }
 
-  if (rank != 0) {
-    input_str.resize(input_length);
-  }
-
-  MPI_Bcast(input_str.data(), input_length, MPI_CHAR, 0, MPI_COMM_WORLD);
-
   int chunk_size = input_length / size;
   int remainder = input_length % size;
 
-  int start_pos = (rank * chunk_size) + std::min(rank, remainder);
-  int end_pos = start_pos + chunk_size + (rank < remainder ? 1 : 0);
+  std::vector<int> sendcounts(size);
+  std::vector<int> displacements(size);
 
-  char left_boundary_char = '\0';
-  if (start_pos > 0) {
-    left_boundary_char = input_str[static_cast<size_t>(start_pos - 1)];
+  if (rank == 0) {
+    for (int i = 0; i < size; ++i) {
+      sendcounts[i] = chunk_size + (i < remainder ? 1 : 0);
+      displacements[i] = (i * chunk_size) + std::min(i, remainder);
+    }
   }
 
-  int local_count = CountSentencesInChunk(input_str, start_pos, end_pos, left_boundary_char);
+  int local_chunk_size = chunk_size + (rank < remainder ? 1 : 0);
+  std::string local_chunk(local_chunk_size, '\0');
+
+  MPI_Scatterv(rank == 0 ? input_str.data() : nullptr, sendcounts.data(), displacements.data(), MPI_CHAR,
+               local_chunk.data(), local_chunk_size, MPI_CHAR, 0, MPI_COMM_WORLD);
+
+  std::vector<char> left_boundary_chars(size);
+  if (rank == 0) {
+    for (int i = 0; i < size; ++i) {
+      int start_pos = (i * chunk_size) + std::min(i, remainder);
+      left_boundary_chars[i] = (start_pos > 0) ? input_str[static_cast<size_t>(start_pos - 1)] : '\0';
+    }
+  }
+
+  char left_boundary_char = '\0';
+  MPI_Scatter(left_boundary_chars.data(), 1, MPI_CHAR, &left_boundary_char, 1, MPI_CHAR, 0, MPI_COMM_WORLD);
+
+  int local_count = CountSentencesInChunk(local_chunk, 0, local_chunk_size, left_boundary_char);
 
   int global_count = 0;
   MPI_Reduce(&local_count, &global_count, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
